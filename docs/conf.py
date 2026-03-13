363aee82bbc8cc6bb04750bb976cc806c61808d7
Swaiku <prin.jeremy@protonmail.ch>
2026-03-13 10:52:09 +0100

    🔖 Bump version: 0.1.4 -> 0.1.5

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 project = "oberon0-runtime"
 copyright = "2026, Jérémy Prin"
 author = "Jérémy Prin"
-release = "0.1.4"
+release = "0.1.5"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
